6ec4b867d25615bbe104f60df7a3490c814c45da
Sriraj Bura <sriraj.bura16@gmail.com>
2021-02-04 05:04:50 -0500

    added missing oprate function

--- calculator.js
+++ calculator.js
@@ -14,3 +14,9 @@ function divide(num1, num2) {
 	return num1 / num2
 }
 
+function operate(operator, num1, num2){
+    if(operator === '+') return add(num1, num2);
+    if(operator === '-') return subtract(num1, num2);
+    if(operator === '*') return multiply(num1, num2);
+    if(operator === '/') return divide(num1, num2);
+}
